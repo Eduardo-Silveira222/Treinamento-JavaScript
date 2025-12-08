279b1df8875c8e2d62b0726a3230c812a0afa076
Eduardo-Silveira222 <dudusoaressilveira@gmail.com>
2025-12-08 16:39:22 -0300

    Exercício 1 finalizado!

--- Repetição/script.js
+++ Repetição/script.js
@@ -1,22 +1,31 @@
-function contar(){
-    var inicio = window.document.getElementById('txti')
-    var fim = window.document.getElementById('txtf')
-    var passo = window.document.getElementById('txtp')
-    var res = window.document.getElementById('res')
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length  == 0){
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
         window.alert('[Erro] Preencha os dados!')
-    }else {
-        res.innerHTML = 'contando:'
-        var i = Number(inicio.value)
-        var f = Number(fim.value)
-        var p = Number(passo.valeu)
-        c = i
-
-        do{
-                res.innerHTML += `${c}`
-                c += p
-            }while(c <= f) 
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p <= 0){
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
         }
+        if(i < f){
+            for(let c = i; c <= f; c+=p) {
+            res.innerHTML += ` ${c} \u{1F449}`
+        }
+       
+        } else{
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
